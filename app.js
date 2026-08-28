dba29a69d5c2e3ee15f5146ff99b01ccfe88e940
Juan <castanedajuan@gmail.com>
2026-08-28 11:52:06 -0500

    Trabajando en el endpoint

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+require("dotenv/config");
+const PUERTO = process.env.PUERTO || 5000;
+//configurar para lectura del archivo
+const sistemaArchivo = require("fs");
+const ruta = require("path");
+const rutaArchivoJson = ruta.join(__dirname, "datos.json");
+
+
+//endpointraiz
+app.get("/", function(req, res) {
+  res.send('API Rest - Aprendices');
+});
+
+//endpoint para ver los datos del archivo
+app.get("/api/aprendices", function(req, res)  {
+  //los datos se pueden traer de una base de datos, de un archivo,etc
+  sistemaArchivo.readFile(rutaArchivoJson, "utf8", function(error, datos) {
+    if (error) {
+      return res.json({error:"Error al leer los datos"});
+    }
+  });
+});
+
+app.post("/api/aprendices", (req, res) => {
+    res.json({mensaje: "Trabajando en el endpoint"})
+  });
+
+app.listen(PUERTO, () =>{
+  console.log(`Servidor en funcionamiento en el puerto: http://localhost:${PUERTO}`);
+});
